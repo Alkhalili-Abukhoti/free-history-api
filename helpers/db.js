a946466b482a92b1700ded2a3b6879ba8c25d925
Khalid Al Khalili <kfkhalili@hotmail.com>
2020-04-05 23:31:33 +0200

    Create user model and controller kfkhalili/free-history-api/2

--- helpers/db.js
+++ helpers/db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 var mongoose = require('mongoose');
 
-var db = mongoose.connect(process.env.MONGODB_URI || 3000, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true 
+var db = mongoose.connect(process.env.MONGODB_URI || 9000, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false 
 });
 mongoose.Promise = global.Promise;
 
---
+++ test/user.test.js
@@ -0,0 +1,104 @@
+require('dotenv').config();
+
+var supertest = require('supertest');
+var should = require('should');
+var server = supertest.agent("http://localhost:9000");
+var assert = require("assert");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+var username = "test" + getRandomInt(10);
+
+var testCred = {
+    "username": username,
+    "password": "123456"
+}
+var testUser = { 
+    ...testCred,
+    "lastName": "User",
+    "firstName": "Test",
+    "email": username + "@domain.com"
+}
+
+///// UNIT TEST BEGIN /////
+
+var token = ""; // unknown until authentication
+describe("User creation and authentication", () => {
+    it(`should be possible to register a new user: ${username}`, (done) => {
+        server.post("/users/register")
+        .send(testUser)
+        .expect("Content-type", /json/)
+        .expect(200) // This is the HTTP response
+        .end(function(err, res){
+            done();
+        });
+    });
+
+    it("should be possible to authenticate the newly created user", (done) => {
+        server.post("/users/authenticate")
+        .send(testCred)
+        .expect("Content-type", /json/)
+        .expect(200)
+        .end((err, res) => {
+            token = res.body.token;
+            assert.equal(token.length, 149);
+            done();
+        });
+    });
+});
+
+var id = ""; // unknown until current user becomes known
+describe("Getting users", () => {
+    it("should be possible to get all users", (done) => {
+        server.get("/users/")
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res) => {
+            assert(res.body.length, 1);
+            done();
+        });
+    });
+
+    it("should be possible to get current user info", (done) => {
+        server.get("/users/current")
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res) => {
+            id = res.body._id;
+            done();
+        });
+    });
+
+    it("should be possible to get user by id", (done) => {
+        server.get("/users/" + id)
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res) => {
+            done();
+        });
+    });
+});
+
+describe("Modifying and deleting users", () => {
+    it("should be possible to modify user info", (done) => {
+        server.put("/users/" + id)
+        .set('Authorization', 'Bearer ' + token)
+        .send({ "email": "changed@domain.com"})
+        .expect(200)
+        .end((err, res) => {
+            done();
+        });
+    });
+
+    it("should be possible to delete created users", (done) => {
+        server.delete("/users/" + id)
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res) => {
+            done();
+        });
+    });
+});
+
